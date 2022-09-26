a622ee2c1928a7961a8c0eda067158df1fb53397
Defalt1878 <ilyaudalov1@mail.ru>
2022-09-27 01:37:48 +0500

    Added CSV parser

---
+++ src/parser/ReportParserCVS.java
@@ -0,0 +1,57 @@
+package parser;
+
+import report.ReportCard;
+import report.Student;
+import report.StudentAchievements;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class ReportParserCVS {
+    private String courseName;
+    private List<String> modules;
+    private List<String> tasks;
+
+    public ReportParserCVS() {
+    }
+
+    public ReportCard parse(String courseName, String path) {
+        this.courseName = courseName;
+        try {
+            return tryParse(path);
+        } catch (IOException e) {
+            throw new RuntimeException("Error happens during parsing", e);
+        }
+    }
+
+    private ReportCard tryParse(String path) throws IOException {
+        var lines = Files.readAllLines(Paths.get(path));
+        modules = getDataStream(lines.get(0)).toList();
+        tasks = getDataStream(lines.get(1)).toList();
+
+        var maxes = getDataStream(lines.get(2)).map(Integer::parseInt).toList();
+        var maxScores = ScoreParser.parse(courseName, modules, tasks, maxes);
+
+        var studentsAchievements = lines.stream()
+            .skip(3)
+            .map(this::parseAchievements)
+            .toList();
+
+        return new ReportCard(maxScores, studentsAchievements);
+    }
+
+    private static Stream<String> getDataStream(String line) {
+        return Arrays.stream(line.split(";")).skip(2);
+    }
+
+    private StudentAchievements parseAchievements(String line) {
+        var data = line.split(";");
+        var student = new Student(data[0], data[1]);
+        var scores = Arrays.stream(data).skip(2).map(Integer::parseInt).toList();
+        return new StudentAchievements(student, ScoreParser.parse(courseName, modules, tasks, scores));
+    }
+}
