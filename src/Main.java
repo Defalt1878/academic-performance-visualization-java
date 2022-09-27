298be1f77961667e58954e1cb8664f32ff398490
Defalt1878 <ilyaudalov1@mail.ru>
2022-09-27 14:20:21 +0500

    Added test main method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import parser.ReportParserCVS;
+import report.Student;
+
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var path = "basicprogramming_2.csv";
+        var parser = new ReportParserCVS();
+        var report = parser.parse("C#", path);
+
+        var sorted = report.sortStudentsByScore().stream()
+            .map(Student::getFullName)
+            .map(student -> String.format("%s\t\t%s", student, report.getStudentScore(student)))
+            .collect(Collectors.joining("\n"));
+        System.out.println(sorted);
     }
 }
