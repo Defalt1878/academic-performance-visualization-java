b03d6ebd49dbc488182cf9da1fd84d98e67cc0a3
Defalt1878 <ilyaudalov1@mail.ru>
2022-09-30 00:50:33 +0500

    Readme written.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import parser.ReportParserCVS;
 
 public class Main {
     public static void main(String[] args) {
-        var path = "basicprogramming_2.csv";
+        var path = "misc/basicprogramming_2.csv";
         var parser = new ReportParserCVS();
         var report = parser.parse("C#", path);
     }
